7e9416d321bfca9c8f9b1fd832a20f9edb0b1390
Alex Nichol <unixpickle@gmail.com>
2023-09-05 12:18:19 -0700

    cleanup test

--- raft/raft_test.go
+++ raft/raft_test.go
@@ -9,21 +9,64 @@ import (
 )
 
 func TestRaftSimpleCase(t *testing.T) {
-	t.Run("Randomized", func(t *testing.T) {
+	t.Run("Latency", func(t *testing.T) {
 		testRaftSimpleCase(t, 5, true)
 	})
-	t.Run("Greedy", func(t *testing.T) {
+	t.Run("Instant", func(t *testing.T) {
 		testRaftSimpleCase(t, 5, false)
 	})
 }
 
 func testRaftSimpleCase(t *testing.T, numNodes int, randomized bool) {
+	env := NewRaftEnvironment(5, 1, randomized)
+	env.Loop.Go(func(h *simulator.Handle) {
+		defer env.Cancel()
+		client := &Client[HashMapCommand]{
+			Handle:      h,
+			Network:     env.Network,
+			Port:        env.Clients[0],
+			Servers:     env.Servers,
+			SendTimeout: 10,
+		}
+		for i := 0; i < 10; i++ {
+			value := "hello" + strconv.Itoa(i)
+			x, err := client.Send(HashMapCommand{Key: strconv.Itoa(i), Value: value}, 0)
+			if err != nil {
+				t.Fatal(err)
+			} else if v := x.(StringResult).Value; v != value {
+				t.Fatalf("expected %#v but got %#v", v, x)
+			}
+			for j := 0; j <= i; j++ {
+				expected := "hello" + strconv.Itoa(i)
+				resp, err := client.Send(HashMapCommand{Key: strconv.Itoa(i)}, 0)
+				if err != nil {
+					t.Fatal(err)
+				} else if v := resp.(StringResult).Value; v != expected {
+					t.Fatalf("expected %#v but got %#v", expected, v)
+				}
+			}
+		}
+	})
+
+	env.Loop.MustRun()
+}
+
+type RaftEnvironment struct {
+	Cancel  func()
+	Servers []*simulator.Port
+	Clients []*simulator.Port
+	Loop    *simulator.EventLoop
+	Network *simulator.OrderedNetwork
+}
+
+func NewRaftEnvironment(numServers, numClients int, randomized bool) *RaftEnvironment {
 	loop := simulator.NewEventLoop()
 	nodes := []*simulator.Node{}
 	ports := []*simulator.Port{}
-	for i := 0; i < numNodes+1; i++ {
-		nodes = append(nodes, simulator.NewNode())
-		ports = append(ports, nodes[len(nodes)-1].Port(loop))
+	for i := 0; i < numServers+numClients; i++ {
+		node := simulator.NewNode()
+		nodes = append(nodes, node)
+		ports = append(ports, node.Port(loop))
 	}
 
 	// The network is always ordered, but may have random latency.
@@ -37,7 +80,7 @@ func testRaftSimpleCase(t *testing.T, numNodes int, randomized bool) {
 
 	context, cancelFn := context.WithCancel(context.Background())
 
-	for i := 0; i < numNodes; i++ {
+	for i := 0; i < numServers; i++ {
 		index := i
 		loop.Go(func(h *simulator.Handle) {
 			var other []*simulator.Port
@@ -64,36 +107,11 @@ func testRaftSimpleCase(t *testing.T, numNodes int, randomized bool) {
 		})
 	}
 
-	// Make sure we can actually push states.
-	clientPort := ports[len(ports)-1]
-	loop.Go(func(h *simulator.Handle) {
-		defer cancelFn()
-		client := &Client[HashMapCommand]{
-			Handle:      h,
-			Network:     network,
-			Port:        clientPort,
-			Servers:     ports[:len(ports)-1],
-			SendTimeout: 10,
-		}
-		for i := 0; i < 10; i++ {
-			value := "hello" + strconv.Itoa(i)
-			x, err := client.Send(HashMapCommand{Key: strconv.Itoa(i), Value: value}, 0)
-			if err != nil {
-				t.Fatal(err)
-			} else if v := x.(StringResult).Value; v != value {
-				t.Fatalf("expected %#v but got %#v", v, x)
-			}
-			for j := 0; j <= i; j++ {
-				expected := "hello" + strconv.Itoa(i)
-				resp, err := client.Send(HashMapCommand{Key: strconv.Itoa(i)}, 0)
-				if err != nil {
-					t.Fatal(err)
-				} else if v := resp.(StringResult).Value; v != expected {
-					t.Fatalf("expected %#v but got %#v", expected, v)
-				}
-			}
-		}
-	})
-
-	loop.MustRun()
+	return &RaftEnvironment{
+		Cancel:  cancelFn,
+		Servers: ports[:numServers],
+		Clients: ports[numServers:],
+		Loop:    loop,
+		Network: network,
+	}
 }
